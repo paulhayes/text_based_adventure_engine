0a2fe8fff46f763d20b8acba08d99bc61c67fb2f
paulhayes <paul.hayes@entropedia.co.uk>
2013-04-29 23:05:16 +0100

    Fixed horrible bug in unregistering commands

--- examples/telnet.js
+++ examples/telnet.js
@@ -8,12 +8,12 @@ var world = new World();
 
 var s = new telnet.Server(function (client) {
 	// I am the connection callback
-	var player = new Player(client);
+	var player = new Player(function(msg){ client.write(msg) });
 	world.addPlayer(player);
 
 	console.log("connected term=%s %dx%d", client.term, client.windowSize[0], client.windowSize[1]);
 
-	client.on('data', function (buf) { player.onData(buf) });
+	client.on('data', function (buf) { player.do(buf) });
 	client.on('resize', function (width, height) {
 	  console.log("resized to %dx%d", width, height);
 	});
